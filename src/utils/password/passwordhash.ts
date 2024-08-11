0390484274ced154ab5808af5a05548576ef09c9
claudio <dinhosilwa_@hotmail.com>
2024-08-11 08:32:17 -0300

    feat: add password hash creator for admin users

---
+++ src/utils/password/passwordhash.ts
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+export const createPassHash = async (pass: string): Promise<string> => {
+  try {
+    const createHash = await bcrypt.hash(pass, saltRounds);
+    return createHash;
+  } catch (error) {
+    throw new Error("Erro ao criar o hash da senha");
+  }
+};
+
+export const comparePassHash = async (
+  pass: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  try {
+    const ismatch = await bcrypt.compare(pass, hashedPassword);
+    if (!ismatch) {
+      console.log("Senhas incompativeis");
+    }
+    return ismatch;
+  } catch (error) {
+    throw new Error("Erro ao criar o hash da senha");
+  }
+};
