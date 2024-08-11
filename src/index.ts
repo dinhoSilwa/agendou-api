77033d9bc40e0d2a1a83bdf210d41cb9db991427
claudio <dinhosilwa_@hotmail.com>
2024-08-11 06:57:01 -0300

    fix: correct Express app creation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { Application } from "express";
+import express, { Application } from "express";
 import { connectDb } from "./config/db";
 import { adminRoute } from "./routes/useradminRoutes/adminroute";
 export const app: Application = express();
-app.use();
 connectDb();
-app.use(express.json())
-app.use("/adminusers", adminRoute)
+app.use(express.json());
+app.use("/adminusers", adminRoute);
