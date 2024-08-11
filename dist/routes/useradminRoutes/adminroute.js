f67cdf4eb7f1f64ab521cd34eb7c37092d549ec2
claudio <dinhosilwa_@hotmail.com>
2024-08-11 09:40:44 -0300

    refator : vercel file

--- dist/routes/useradminRoutes/adminroute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.adminRoute = void 0;
-const express_1 = require("express");
-const admin_1 = require("../../controllers/admin-controllers/admin");
-exports.adminRoute = (0, express_1.Router)();
-exports.adminRoute.post("/createAdmin", admin_1.createAdmin);
-//# sourceMappingURL=adminroute.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./index";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Servidor Rodando na Porta : ${PORT}`);
