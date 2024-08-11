b480aaa29fde2922c15e2b94216ba8856f5d9acc
claudio silva <claudiosilva.one@gmail.com>
2024-08-11 19:09:09 -0300

    Fix : builds files

--- dist/routes/useradminRoutes/adminroute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.adminRoute = void 0;
-const express_1 = require("express");
-const admin_1 = require("../../controllers/admin-controllers/admin");
-exports.adminRoute = (0, express_1.Router)();
-exports.adminRoute.post("/createAdmin", admin_1.createAdmin);
-exports.adminRoute.post('/get-admin', admin_1.getAdmin);
-//# sourceMappingURL=adminroute.js.map
--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-const PORT = process.env.PORT || 3001;
-index_1.app.listen(PORT, () => {
-    console.log(`Servidor Rodando na Porta : ${PORT}`);
-});
-//# sourceMappingURL=server.js.map
