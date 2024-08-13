2d955e8a1f351e50e02242c227a389a33d1dde4f
claudio silva <claudiosilva.one@gmail.com>
2024-08-13 08:36:32 -0300

    CHORE : create build config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import { connectDb } from "./config/db";
 import { adminRoute } from "./routes/useradminRoutes/adminroute";
-import cors from 'cors'
+import cors from "cors";
 export const app: Application = express();
 connectDb();
 app.use(express.json());
 
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use("/adminusers", adminRoute);
-
+app.get("/", (req: Request, res: Response) => {
+  res.send("Rodou");
+});
